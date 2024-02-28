54821dd264d56ea6430a7eff430415b0b6450980
Kameswar Panda <kameswarpanda11@gmail.com>
2024-02-28 23:59:02 +0530

    Added Type Conversion question

---
+++ TypeConversion/tcQ1.java
@@ -0,0 +1,11 @@
+//Predict the output
+
+package TypeConversion;
+
+public class tcQ1 {
+    public static void main(String args[]) { 
+        int rollNo = 101; 
+        float newRollNo = rollNo; 
+        System.out.println(newRollNo); 
+    } 
+}
---
+++ TypeConversion/tcQ2.java
@@ -0,0 +1,9 @@
+package TypeConversion;
+
+public class tcQ2 {
+    public static void main(String args[]) { 
+        short number = 32767; 
+        short numberTwo = (short)(number + 1); 
+        System.out.println(numberTwo); 
+    } 
+}
