2527404e0200a1ec4fdc3c45a165cb817550f6e8
Kameswar Panda <kameswarpanda11@gmail.com>
2024-03-06 03:22:14 +0530

    more questions

---
+++ Agregation/Building.java
@@ -0,0 +1,29 @@
+// Predict the output
+
+package Agregation;
+
+class Room {
+  private int roomNo;
+
+  public Room(int n) {
+    System.out.println("Room created");
+    this.roomNo = n;
+  }
+}
+
+public class Building {
+  public Room room;
+
+  public Building() {
+    this.room = new Room(20);
+  }
+
+  public Building(Room room) {
+    System.out.println("Building created");
+    this.room = room;
+  }
+
+  public static void main(String args[]) {
+    Building b = new Building();
+  }
+}
